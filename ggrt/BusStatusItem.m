0fccae7307e56c1fa836c8ec480a506e5463fb9b
Jerry Yu <hweetty@gmail.com>
2015-01-31 16:19:00 -0500

    missed file

---
+++ ggrt/BusStatusItem.m
@@ -0,0 +1,14 @@
+//
+//  BusStatusItem.m
+//  ggrt
+//
+//  Created by Jerry Yu on 2015-01-31.
+//  Copyright (c) 2015 Jerry Yu. All rights reserved.
+//
+
+#import "BusStatusItem.h"
+
+@implementation BusStatusItem
+
+
+@end
